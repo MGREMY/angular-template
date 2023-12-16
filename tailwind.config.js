3eb525c73aef7640eab2845708ab22256c2ef07c
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-16 23:11:17 +0100

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+export const content = ['./src/**/*.{html,ts}'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
