ca2449e00d2e9c1ae717029a509a60a320034e13
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-16 21:57:23 +0100

    Update esLint and Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { importProvidersFrom } from "@angular/core";
-import { AppComponent } from "./app/app.component";
-import { AppRoutingModule } from "./app/app-routing.module";
-import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(AppComponent, {
   providers: [importProvidersFrom(BrowserModule, AppRoutingModule)],
