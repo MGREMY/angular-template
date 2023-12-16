3678c11fe9b4eece5190f1b6af2bb5aa29940643
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-16 21:59:16 +0100

    esLint on the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from "@angular/common";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet],
 })
 export class AppComponent {
-  title = "angular-template";
+  title = 'angular-template';
   constructor() {
-    console.log("hello world !");
+    console.log('hello world !');
   }
 }
