5ba7f8a7d1674f824aafee5893bc09d95558cfb9
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-16 23:23:58 +0100

    Update TailwindCSS config

---
+++ .postcss.config.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+export const plugins = {
+  'postcss-import': {},
+  'tailwindcss/nesting': 'postcss-nesting',
+  'postcss-preset-env': {
+    features: { 'nesting-rules': false },
+  },
+  tailwindcss: {},
+  autoprefixer: {},
+};
