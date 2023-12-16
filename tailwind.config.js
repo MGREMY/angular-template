f513eb903de591a6c1863c57e75bcab9bcd56fa9
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-17 00:45:45 +0100

    Rollback on config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,10 @@
  * @type {import('tailwindcss').Config}
  */
 
-export const content = [
-  './src/**/*.{html,ts}',
-  './node_modules/flowbite/**/*.js',
-];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './node_modules/flowbite/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
 };
-export const plugins = [require('flowbite/plugin')];
